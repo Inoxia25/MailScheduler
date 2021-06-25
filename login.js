203bd265e83e3bcab66d6f7e1ecb1c47eb629b40
ritikakhandelwal-17 <83296933+ritikakhandelwal-17@users.noreply.github.com>
2021-06-25 22:31:16 +0530

    Add files via upload
    login

---
+++ login.js
@@ -0,0 +1,12 @@
+function signup()
+{
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("signup-form").style.display = "block";
+}
+
+function login()
+{
+    document.getElementById("signup-form").style.display = "none";
+    document.getElementById("login-form").style.display = "block";
+    
+}
