91e2e96e94c46297efac4cb337bda7a7a747c922
Inoxia25 <nandinisapna1401@gmail.com>
2021-06-27 19:45:22 +0530

    added history.js

---
+++ models/history.js
@@ -0,0 +1,14 @@
+var mongoose=require("mongoose");
+var hisSchema=new mongoose.Schema({
+	author: {id: { type: mongoose.Schema.Types.ObjectId,
+				 ref:"User"},username:String}, 
+                 mailfrom:String,
+    mailto:String,
+    mailcc:String,
+    subject:String,
+    body:String,
+	schedule: String,
+time:String,
+date:String,
+day:String});
+module.exports= mongoose.model("History",hisSchema); 
